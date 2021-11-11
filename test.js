b5330e773e4832e0929d5e3e55a87583d8194ed7
AdrienStas <adrien.stas@student.ehb.be>
2021-11-11 21:14:19 +0100

    Unit testen

--- test.js
+++ test.js
@@ -1,19 +1,52 @@
 import { assert } from "chai"
 import supertest from "supertest"
+import * as database from './database.js'
+import mongoDb from 'mongodb'
+const mongoClient = mongoDb.MongoClient
 
 const request = supertest("http://localhost:9000")
 
-
-
-describe("inzendingen api", ()=>{
-    async function getId(){
-        let inzendingen = (await request.get("/inzendingen")).body
-        for (let inzending of inzendingen){
-            if (inzending != undefined) return inzending.id
-        }
-        return undefined
+async function getId(){
+    let inzendingen = (await request.get("/inzendingen")).body
+    for (let inzending of inzendingen){
+        if (inzending != undefined) return inzending.id
     }
+    return undefined
+}
 
+
+describe("unit tests: inzendingen API", ()=>{
+    it("insert inzending", async ()=>{
+        let inzending = {
+            naam: "Adrien",
+            email: "adrien.stas@student.ehb.be",
+            opdracht: "voetbal trucje",
+            link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        }
+        let addedInzending = await database.insertInzending(mongoClient,inzending)
+        assert.notEqual(addedInzending,undefined)
+    })
+    it("update inzending", async ()=>{
+        let id = await getId()
+        let inzending = {
+            naam: "Adrien",
+            email: "adrien.stas@student.ehb.be",
+            opdracht: "voetbal trucje 2",
+            link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        }
+        await database.updateInzending(mongoClient, id, inzending)
+        let updatedInzending = await database.getInzeding(mongoClient,id)
+        assert.equal(updatedInzending.opdracht,"voetbal trucje 2")
+    })
+    it("delete inzending test",async ()=>{
+        let id = await getId()
+        await database.deleteInzending(mongoClient,id)
+        let inzending = await database.getInzeding(mongoClient,id)
+        assert.equal(inzending,undefined)
+    })
+})
+
+describe("integration tests: inzendingen api", ()=>{
     it("POST /insert", () =>{
         let inzending = {
             naam: "Adrien",
@@ -76,4 +109,3 @@ describe("inzendingen api", ()=>{
         })
     })
 })
-
